f3bb520728bcb2cefa2014b002c4ebdb90c2efe3
Leifer Mendez <leifer33@gmail.com>
2020-05-21 17:05:48 +0200

    Fix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::loadKeysFrom(storage_path());
         //
     }
 }
